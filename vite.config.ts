be8f0363269421eae96c9e4f067a5a0cb614be96
fogelt <fogeltt@gmail.com>
2025-12-14 04:14:30 +0100

    reverted to tailwind v3

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
 // vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from '@tailwindcss/vite'
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    tailwindcss()],
+    react()
+  ],
+
+  css: {
+    postcss: path.resolve(__dirname, 'postcss.config.cjs'),
+  },
 
   resolve: {
     alias: {
